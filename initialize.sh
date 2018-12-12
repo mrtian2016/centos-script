50a54217dd3d41c59cf6abdca0446eb3b341c9c7
田田田田田先森丶 <tjy_we@163.com>
2018-12-12 11:41:41 +0800

    初始化脚本

---
+++ initialize.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# 检查是否为root用户
+if [ $(id -u) != "0" ]; then
+    echo "Error: You must be root to run this script, please use root to install software"
+    exit 1
+fi
+
+# 切换到用户目录
+cd ~
+
+# 安装第三方源
+echo "Installing third-party sources..."
+yum -y install epel-release
+
+#安装yum-axelge,安装软件时可以并行下载
+echo "Installing yum-axelget..."
+yum -y install yum-axelget
+
+# 更新软件
+yum -y update
+
+# 安装 zsh wget screen git
+echo "Installing zsh wget screen git..."
+yum -y install zsh wget screen git vim
+
+# 切换 shell
+chsh -s /bin/zsh
+
+# 安装 Oh-My-Zsh
+echo "Installing Oh-My-Zsh..."
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+
+# 一些alias
+echo "Writing some alias..."
+echo "alias vi=vim" >> ~/.zshrc
+echo -e "alias reload=\"source ~/.zshrc && echo '>> OH MY, ZSH configurations are reloaded! '\"" >> ~/.zshrc
+# 更改语言
+echo -e "LANG=\"en_US.UTF-8\"" > /etc/default/local
+echo "Complete !!! Have Fun !!!"
+
