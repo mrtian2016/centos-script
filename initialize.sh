5f02f2c122d071cf5080041d8b2d4af37a13a247
Martin <mrtian2016@gmail.com>
2021-09-16 09:13:24 +0800

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -100,6 +100,6 @@ fi
 
 if [ "$install_lnmp" = true ]; then
     echo "安装$lnmp"
-    wget http://soft.vpser.net/lnmp/lnmp1.5.tar.gz -cO lnmp1.5.tar.gz && tar zxf lnmp1.5.tar.gz && cd lnmp1.5 && ./install.sh "$lnmp"
+    wget http://soft.vpser.net/lnmp/lnmp1.8.tar.gz -cO lnmp1.8.tar.gz && tar zxf lnmp1.8.tar.gz && cd lnmp1.8 && ./install.sh "$lnmp"
 fi
 
